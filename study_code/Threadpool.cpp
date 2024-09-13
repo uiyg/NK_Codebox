b00c13317fbda2e0cd7a30ebf54e230725f6b9b7
uiyg <2361102162@qq.com>
2024-09-13 16:00:06 +0800

    modified:   study_code/Threadpool.cpp

--- study_code/Threadpool.cpp
+++ study_code/Threadpool.cpp
@@ -1,13 +1,19 @@
+// 本程序演示了线程池的使用。
+// 它创建一个指定数量的线程池，并将任务添加到池中执行。
+// 任务在池中的线程中执行。
+// 本程序使用C++11标准库中的多线程和同步功能。
 #include <iostream>
 #include <mingw.mutex.h>
 #include <mingw.condition_variable.h>
 #include <mingw.thread.h>
 #include <queue>
 #include <functional>
-// ???
+
+// Threadpool类代表一个线程池。
 class Threadpool
 {
 public:
+    // 构造函数创建一个指定数量的线程池。
     Threadpool(int numThread) : stop(false)
     {
         for (int i = 0; i < numThread; i++)
@@ -27,6 +33,7 @@ public:
                 } });
         }
     }
+    // 析构函数停止池中的所有线程。
     ~Threadpool()
     {
         {
@@ -41,6 +48,7 @@ public:
             }
         }
     }
+    // enqueue函数将任务添加到池的队列中。
     template <typename F, typename... Args>
     void enqueue(F &&f, Args &&...args)
     {
@@ -59,15 +67,16 @@ private:
     std::condition_variable condition;
     bool stop;
 };
+// main函数创建一个4个线程的池，并添加8个任务。
 int main()
 {
     Threadpool pool(4);
     for (int i = 0; i < 8; i++)
     {
         pool.enqueue([i]
-                     { printf("Task %d:is running! thradID:%d\n", i, std::this_thread::get_id());
+                     { printf("任务%d正在运行！线程ID：%d\n", i, std::this_thread::get_id());
             std::this_thread::sleep_for(std::chrono::seconds(1));
-            printf("Task %d:is end! thradID:%d\n", i, std::this_thread::get_id()); });
+            printf("任务%d已完成！线程ID：%d\n", i, std::this_thread::get_id()); });
     }
     return 0;
 }
